92714c31b55a20869006b1ec9d81cd0834e77e69
Ken-Chy129 <ken-chy129@qq.com>
2024-08-17 17:15:53 +0800

    虚拟线程测试

---
+++ new_jdk/src/main/java/cn/ken/CreateVirtualThread.java
@@ -0,0 +1,36 @@
+package cn.ken;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CreateVirtualThread {
+
+    public void createByThread() throws InterruptedException {
+        // name(String prefix, Integer start) p0:前缀 p1:计数器初始值
+        Thread.Builder.OfVirtual virtualThreadBuilder = Thread.ofVirtual().name("worker-", 0);
+        Thread worker0 = virtualThreadBuilder.start(this::doSomethings);
+        worker0.join();
+        System.out.print("finish worker-0 running");
+        Thread worker1 = virtualThreadBuilder.start(this::doSomethings);
+        worker1.join();
+        System.out.print("finish worker-1 running");
+    }
+
+    public void createByExecutors() throws ExecutionException, InterruptedException {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<?> submit = executorService.submit(this::doSomethings);
+            submit.get();
+            System.out.print("finish running");
+        }
+    }
+
+    public static void main(String[] args) {
+        CreateVirtualThread createVirtualThread = new CreateVirtualThread();
+    }
+
+    private void doSomethings() {
+        System.out.print("hello virtual");
+    }
+}
--- new_jdk/src/main/java/cn/ken/Main.java
+++
@@ -1,8 +0,0 @@
-package cn.ken;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.print("dd");
-    }
-}
