f89c6c5515a6eef4bda8fffacd0c0526caaee810
Ken-Chy129 <ken-chy129@qq.com>
2024-08-18 12:24:04 +0800

    使用信号量限制虚拟线程并发数

---
+++ new_jdk/src/main/java/cn/ken/TestSemaphore.java
@@ -0,0 +1,32 @@
+package cn.ken;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author Ken-Chy129
+ * @date 2024/8/17
+ */
+public class TestSemaphore {
+    public static void main(String[] args) {
+        Semaphore sem = new Semaphore(10);
+        // ......
+        Executors.newVirtualThreadPerTaskExecutor().submit(() -> {
+            try {
+                // 执行任务前信号量-1，表示多了一个并发线程在执行了，剩下的可同时执行数量减少
+                // 如果信号量（许可证）为0，则阻塞直到有其他线程执行完成释放许可证
+                sem.acquire();
+                doSomething();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                // 执行完任务后信号量+1
+                sem.release();
+            }
+        });
+    }
+
+    private static void doSomething() {
+
+    }
+}
